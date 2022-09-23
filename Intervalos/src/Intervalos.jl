e1f5fd0e1c30dc6ffba740e2c63d60530ccd7cc6
Luis Benet <benet@icf.unam.mx>
2022-09-22 20:11:04 -0500

    Agrego Project.toml y src/Intervalos.jl

---
+++ Intervalos/src/Intervalos.jl
@@ -0,0 +1,13 @@
+#=
+Este es el archivo que define al módulo `Intervalos`, la paquetería
+que haremos durante el curso, y que deberá cargar todos los
+archivos que generan para sus tareas.
+=#
+
+module Intervalos
+
+# Deben descomentar y cambiar para que se carguen apropiadamente
+# sus archivos, la(s) siguientes líneas:
+# include("zYo/intervalos.jl")
+
+end
