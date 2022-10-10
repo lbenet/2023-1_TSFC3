30f0a29f6213c55c02e4ef0305e5b6dea76743f2
Luis Eduardo Ramírez Montoya <luiseduardoramirezmontoya@ciencias.unam.mx>
2022-10-10 11:33:13 -0500

    Correcciones

--- Intervalos/src/lerm/intervalos.jl
+++ Intervalos/src/lerm/intervalos.jl
@@ -1,3 +1,7 @@
+export Intervalo, ..
+export intervalo_vacio, ⪽, hull, ⊔
+export division_extendida
+
 @doc raw"""
     Intervalo{T <: AbstractFloat}
 
@@ -10,146 +14,167 @@ struct Intervalo{T <: AbstractFloat}
     supremo::T
     # Constructor interno 
     function Intervalo{T}(infimo::T, supremo::T; warn = true) where {T <: AbstractFloat}
-        infimo ≤ supremo && return new{T}(infimo, supremo)
-        if warn
-            @warn("No se cumple que infimo ($infimo) < supremo ($supremo), regresando el intervalo vacío")
-        end
-        return new{T}(convert(T, Inf), convert(T, -Inf))
+        (infimo ≤ supremo) && return new{T}(infimo, supremo)
+        warn && @warn("No se cumple que infimo ($infimo) < supremo ($supremo), regresando el intervalo vacío")
+        return new{T}(T(Inf), T(-Inf))
     end
 end
 
 # Constructores externos
 Intervalo(infimo::T, supremo::T) where {T <: AbstractFloat} = Intervalo{T}(infimo, supremo)
+
 function Intervalo(infimo::T, supremo::S) where {T, S <: Real} 
-    a, b, c = promote(infimo, supremo, 0.)
+    a, b, _ = promote(infimo, supremo, 0.)
     return Intervalo(a, b)
 end
-Intervalo(a::T) where {T <: AbstractFloat} = Intervalo(a, a)
+
+Intervalo(a::T) where {T <: Real} = Intervalo(a, a)
 
 function ..(infimo::T, supremo::S) where {T, S <: Real} 
     return Intervalo(infimo, supremo)
 end
 
 # Promoción
-import Base: promote, zero, iszero, isempty
+import Base: promote
 
 function promote(a::Intervalo{T}, b::Intervalo{S}) where {T, S <: AbstractFloat}
     U = promote_type(T, S)
-    a1 = convert(U, a.infimo)
-    a2 = convert(U, a.supremo)
-    b1 = convert(U, b.infimo)
-    b2 = convert(U, b.supremo)
+    a1 = U(a.infimo)
+    a2 = U(a.supremo)
+    b1 = U(b.infimo)
+    b2 = U(b.supremo)
     return Intervalo(a1, a2), Intervalo(b1, b2)
 end
 
-function promote(a::T, b::Intervalo{S}) where {T, S <: AbstractFloat}
-    U = promote_type(T, S)
-    a_ = convert(U, a)
-    b1 = convert(U, b.infimo)
-    b2 = convert(U, b.supremo)
-    return a_, Intervalo{U}(b1, b2; warn = false)
+# Intervalo vacío
+import Base: isempty
+
+@doc raw"""
+    intervalo_vacio(S::Type{T}) where {T <: AbstractFloat}
+    intervalo_vacio(a::Intervalo{T}) where {T <: AbstractFloat}
+
+Regresa el intervalo vacío **[Inf, -Inf]** del tipo apropiado. 
+"""
+function intervalo_vacio(S::Type{T}) where {T <: AbstractFloat}
+    return Intervalo{S}(S(Inf), S(-Inf); warn = false)
 end
 
-# Intervalo vacío
-intervalo_vacio(T::Type{S}) where {S <: AbstractFloat} = Intervalo{T}(convert(T, Inf), convert(T, -Inf); warn = false)
 intervalo_vacio(a::Intervalo{T}) where {T <: AbstractFloat} = intervalo_vacio(T)
-isempty(a::Intervalo{T}) where {T <: AbstractFloat} = a.infimo == convert(T, Inf) && a.supremo == convert(T, -Inf)
+
+function isempty(a::Intervalo)
+    return (a.supremo < a.infimo)
+end
 
 # Intervalo cero 
+import Base: zero, iszero
+
 zero(::Type{Intervalo{T}}) where {T <: AbstractFloat} = Intervalo(zero(T), zero(T))
-iszero(a::Intervalo{T}) where {T <: AbstractFloat} = a.infimo == zero(T) && a.supremo == zero(T)
+
+zero(a::Intervalo{T}) where {T <: AbstractFloat} = zero(Intervalo{T})
+
+function iszero(a::Intervalo)
+    return iszero(a.infimo) && iszero(a.supremo)
+end
 
 # Relaciones de conjuntos
 import Base: ==, ⊆, ∪, ∩, ∈
 
-function ==(a::Intervalo{T}, b::Intervalo{T}) where {T <: AbstractFloat}
-    return a.infimo == b.infimo && a.supremo == b.supremo
+function ==(a::Intervalo, b::Intervalo)
+    return (a.infimo == b.infimo) && (a.supremo == b.supremo)
 end
 
-function ⊆(a::Intervalo{T}, b::Intervalo{T}) where {T <: AbstractFloat}
-    return a.infimo ≥ b.infimo && a.supremo ≤ b.supremo
+function ⊆(a::Intervalo, b::Intervalo)
+    return (a.infimo ≥ b.infimo) && (a.supremo ≤ b.supremo)
 end
 
-function ⊂(a::Intervalo{T}, b::Intervalo{T}) where {T <: AbstractFloat}
-    return a ⊆ b && a != b
+function ⊂(a::Intervalo, b::Intervalo)
+    return (a ⊆ b) && (a != b)
 end
 
-function ⪽(a::Intervalo{T}, b::Intervalo{T}) where {T <: AbstractFloat}
-    return a.infimo > b.infimo && a.supremo < b.supremo
+function ⪽(a::Intervalo, b::Intervalo)
+    return (a.infimo > b.infimo) && (a.supremo < b.supremo)
 end
 
-function hull(a::Intervalo{T}, b::Intervalo{T}) where {T <: AbstractFloat}
+function hull(a::Intervalo, b::Intervalo)
     return Intervalo(min(a.infimo, b.infimo), max(a.supremo, b.supremo))
 end
 
 const ⊔ = hull
 
-∪(a::Intervalo{T}, b::Intervalo{T}) where {T <: AbstractFloat} = hull(a, b)
+∪(a::Intervalo, b::Intervalo) = hull(a, b)
 
-function ∩(a::Intervalo{T}, b::Intervalo{T}) where {T <: AbstractFloat}
-    if b.supremo < a.infimo || a.supremo < b.infimo
-        return intervalo_vacio(T)
+function ∩(a::Intervalo{T}, b::Intervalo{S}) where {T, S <: AbstractFloat}
+    U = promote_type(T, S)
+    if (b.supremo < a.infimo) || (a.supremo < b.infimo)
+        return intervalo_vacio(U)
     else
         return Intervalo(max(a.infimo, b.infimo), min(a.supremo, b.supremo))
     end
 end
 
-∈(a::T, b::Intervalo{T}) where {T <: AbstractFloat} = (b.infimo ≤ a ≤ b.supremo)
+function ∈(a::T, b::Intervalo) where {T <: Real} 
+    isinf(a) && return false
+    return (b.infimo ≤ a ≤ b.supremo)
+end
 
 # Aritmética
 
 import Base: +, -, *, /, inv, ^
 
-function +(a::Intervalo{T}, b::Intervalo{T}) where {T <: AbstractFloat}
+function +(a::Intervalo, b::Intervalo)
     return Intervalo(prevfloat(a.infimo + b.infimo), nextfloat(a.supremo + b.supremo))
 end
 
-function +(a::T, b::Intervalo{T}) where {T <: AbstractFloat}
+function +(a::T, b::Intervalo) where {T <: Real}
     return Intervalo(prevfloat(a + b.infimo), nextfloat(a + b.supremo))
 end
 
-function -(a::Intervalo{T}, b::Intervalo{T}) where {T <: AbstractFloat}
++(a::Intervalo, b::T) where {T <: Real} = +(b, a)
+
+function -(a::Intervalo, b::Intervalo)
     return Intervalo(prevfloat(a.infimo - b.supremo), nextfloat(a.supremo - b.infimo))
 end
 
-function -(a::T, b::Intervalo{T}) where {T <: AbstractFloat}
+function -(a::T, b::Intervalo) where {T <: Real}
     return Intervalo(prevfloat(a - b.supremo), nextfloat(a - b.infimo))
 end
 
-function -(a::Intervalo{T}, b::T) where {T <: AbstractFloat}
+function -(a::Intervalo, b::T) where {T <: Real}
     return Intervalo(prevfloat(a.infimo - b), nextfloat(a.supremo - b))
 end
 
--(a::Intervalo{T}) where {T, S <: AbstractFloat} = Intervalo(-a.supremo, -a.infimo)
+-(a::Intervalo) = Intervalo(-a.supremo, -a.infimo)
 
-function *(a::Intervalo{T}, b::Intervalo{T}) where {T <: AbstractFloat}
-    ( isempty(a) || isempty(b) ) && return intervalo_vacio(T)
-    ( iszero(a) || iszero(b) ) && return zero(Intervalo{T})
+function *(a::Intervalo{T}, b::Intervalo{S}) where {T, S <: AbstractFloat}
+    U = promote_type(T, S)
+    ( isempty(a) || isempty(b) ) && return intervalo_vacio(U)
+    ( iszero(a) || iszero(b) ) && return zero(Intervalo{U})
     x = (a.infimo * b.infimo, a.infimo * b.supremo, a.supremo * b.infimo, a.supremo * b.supremo)
     return Intervalo(prevfloat(minimum(x)), nextfloat(maximum(x)))
 end
 
-function *(a::T, b::Intervalo{T}) where {T <: AbstractFloat}
+function *(a::T, b::Intervalo) where {T <: Real}
     x = a * b.infimo
     y = a * b.supremo
     return Intervalo(prevfloat(min(x, y)), nextfloat(max(x, y)))
 end
 
-function /(a::Intervalo{T}, b::Intervalo{T}) where {T <: AbstractFloat}
+*(a::Intervalo, b::T) where {T <: Real} = *(b, a)
+
+function /(a::Intervalo{T}, b::Intervalo{S}) where {T, S <: AbstractFloat}
+    U = promote_type(T, S)
+    ( isempty(a) || isempty(b) || iszero(b)) && return intervalo_vacio(U)
     if 0 ∉ b
-        ( isempty(a) || isempty(b) ) && return intervalo_vacio(T)
         x = (a.infimo / b.infimo, a.infimo / b.supremo, a.supremo / b.infimo, a.supremo / b.supremo)
         return Intervalo(prevfloat(minimum(x)), nextfloat(maximum(x)))
-    elseif iszero(b)
-        return intervalo_vacio(T)
     elseif b.infimo < 0 < b.supremo
-        return Intervalo(convert(T, -Inf), convert(T, Inf))
+        return Intervalo(U(-Inf), U(Inf))
     else
         return division_extendida(a, b)[1]
     end
 end
 
-function /(a::T, b::Intervalo{S}) where {T <: Real, S <: AbstractFloat}
+function /(a::T, b::Intervalo) where {T <: Real}
     return Intervalo(a, a) / b
 end
 
@@ -157,42 +182,11 @@ function inv(b::Intervalo{T}) where {T <: AbstractFloat}
     return Intervalo(one(T), one(T)) / b
 end
 
-for op ∈ (:(==), :(⊆), :(⊂), :(⪽), :(hull), :(∪), :(∩), :(+), :(-), :(*), :(/))
-    @eval begin
-
-        function $op(a::Intervalo{T}, b::Intervalo{S}) where {T, S <: AbstractFloat}
-            a_, b_ = promote(a, b)
-            return $op(a_, b_)
-        end
-
-    end
-end 
-
-for op ∈ (:(∈), :(+), :(-), :(*))
-    @eval begin
-
-        function $op(a::T, b::Intervalo{S}) where {T <: Real, S <: AbstractFloat}
-            a_, b_ = promote(a, b)
-            return $op(a_, b_)
-        end
-
-        if $op == :(-)
-            function $op(a::Intervalo{T}, b::S) where {T <: AbstractFloat, S <: Real}
-                a_, b_ = promote(a, b)
-                return $op(a_, b_)
-            end
-        elseif $op != :(∈)
-            $op(a::Intervalo{T}, b::S) where {T <: AbstractFloat, S <: Real} = $op(b, a)
-        end
-
-    end
-end 
-
 # Potencias enteras 
 
 function ^(a::Intervalo{T}, b::Int) where {T <: AbstractFloat}
 
-    isempty(a) && return intervalo_vacio(T)
+    isempty(a) && return intervalo_vacio(a)
 
     if b == 1
         return a
@@ -201,9 +195,9 @@ function ^(a::Intervalo{T}, b::Int) where {T <: AbstractFloat}
         
         if iseven(b)
             if 0 ∈ a
-                return Intervalo(0, nextfloat(y))
+                return Intervalo(zero(T), nextfloat(y))
             else
-                return Intervalo(prevfloat(x), nextfloat(y)) ∩ Intervalo(0, Inf)
+                return Intervalo(prevfloat(x), nextfloat(y)) ∩ Intervalo(zero(T), T(Inf))
             end
         else
             return Intervalo(prevfloat(x), nextfloat(y))
@@ -211,35 +205,51 @@ function ^(a::Intervalo{T}, b::Int) where {T <: AbstractFloat}
     end
 end
 
-# División estendida
+# División extendida
 
-function division_extendida(a::Intervalo{T}, b:: Intervalo{T}) where {T <: AbstractFloat}
+@doc raw"""
+    division_extendida(a::Intervalo{T}, b:: Intervalo{S}) where {T, S <: AbstractFloat}
+
+Regresa una tupla de dos intervalos que corresponde a la división extendida de **a** y **b**.
+Ver la sección 2.3.4 de Tucker, W. (2011). Validated Numerics. Estados Unidos: Princeton University
+Press.
+"""
+function division_extendida(a::Intervalo{T}, b:: Intervalo{S}) where {T, S <: AbstractFloat}
+    U = promote_type(T, S)
     if 0 ∉ b
         return (a / b,)
     elseif (0 ∈ a) && (0 ∈ b)
-        return (Intervalo(convert(T, -Inf), convert(T, Inf)),)
+        return (Intervalo(U(-Inf), U(Inf)),)
     elseif (a.supremo < 0) && (b.infimo < b.supremo == 0)
-        return (Intervalo(a.supremo / b.infimo, convert(T, Inf)),)
+        return (Intervalo(a.supremo / b.infimo, U(Inf)),)
     elseif (a.supremo < 0) && (b.infimo < 0 < b.supremo)
-        return (Intervalo(a.supremo / b.infimo, convert(T, Inf)), Intervalo(convert(T, -Inf), a.supremo / b.supremo))
+        return (Intervalo(a.supremo / b.infimo, U(Inf)), Intervalo(U(-Inf), a.supremo / b.supremo))
     elseif (a.supremo < 0) && (0 == b.infimo < b.supremo)
-        return (Intervalo(convert(T, -Inf), a.supremo / b.supremo),)
+        return (Intervalo(U(-Inf), a.supremo / b.supremo),)
     elseif (a.infimo > 0) && (b.infimo < b.supremo == 0)
-        return (Intervalo(convert(T, -Inf), a.infimo / b.infimo),)
+        return (Intervalo(U(-Inf), a.infimo / b.infimo),)
     elseif (a.infimo > 0) && (b.infimo < 0 < b.supremo)
-        return (Intervalo(convert(T, -Inf), a.infimo / b.infimo), Intervalo(a.infimo / b.supremo, convert(T, Inf)))
+        return (Intervalo(U(-Inf), a.infimo / b.infimo), Intervalo(a.infimo / b.supremo, U(Inf)))
     elseif (a.infimo > 0) && (0 == b.infimo < b.supremo)
-        return (Intervalo(a.infimo / b.supremo, convert(T, Inf)),)
+        return (Intervalo(a.infimo / b.supremo, U(Inf)),)
     elseif (0 ∉ a) && iszero(b)
-        return (intervalo_vacio(T),)
+        return (intervalo_vacio(U),)
     end
 end
 
-
+# Fancy print
 import Base: show
 
-show(io::IO, a::Intervalo{T}) where {T <: AbstractFloat} = print(io, "[", a.infimo, ", ", a.supremo, "]")
+const infs = ["∞", "-∞"]
 
-export Intervalo, ..
-export intervalo_vacio, ⪽, hull, ⊔
-export division_extendida
+function inf_string(x::T) where {T <: AbstractFloat}
+    (x == T(Inf)) && return infs[1]
+    (x == T(-Inf)) && return infs[2]
+    return string(x)
+end
+
+function show(io::IO, a::Intervalo)
+    a1 = inf_string(a.infimo)
+    a2 = inf_string(a.supremo)
+    print(io, "[", a1, ", ", a2, "]")
+end
