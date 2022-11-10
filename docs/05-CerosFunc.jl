bb9be4b668d99466e969a1742a8ae75d9f09cd1a
Luis Benet <benet@icf.unam.mx>
2022-11-09 19:08:41 -0600

    Se corrije el texto del archivo sobre ceros de funciones
    [ci skip]

--- docs/05-CerosFunc.jl
+++ docs/05-CerosFunc.jl
@@ -11,9 +11,10 @@
 #-
 # El método de bisección para números reales (o números de punto flotante), requiere de dos valores iniciales, $a_0$ y $b_0$, tales que la función $f$ evaluada en estos puntos tenga signos contrarios, es decir, $f(a_0) f(b_0) < 0$. A partir de esto, que puede ser difícil en sí de obtener, uno considera el punto medio, $c=(a_0+b_0)/2$.
 #
-# - (i) Si $f(c)=0$, la raíz es $c$, y se termina el procedimiento.
-# - (ii) Si $f(c)$ tiene el mismo signo que $f(a_0)$ entonces definimos $a_1=c$ y $b_1=b$. Si $f(c)$ tiene el mismo signo que $f(b_0)$, entonces definimos $a_1=b$ y $b_1=c$. Con estos valores, se vuelve al inicio del método.
-#
+# i. Si $f(c)=0$, la raíz es $c$, y se termina el procedimiento.
+# ii. Si $f(c)$ tiene el mismo signo que $f(a_0)$ entonces definimos $a_1=c$ y $b_1=b_0$. Si $f(c)$ tiene el mismo signo que $f(b_0)$, entonces definimos $a_1=a_0$ y $b_1=c$. Con estos valores, se vuelve al inicio del método.
+
+#-
 # El método se itera hasta que la distancia entre los extremos $|a_k-b_k|<\tau$, donde $\tau$ es una tolerancia para aproximar el cero de la función que se especifica inicialmente.
 
 #-
@@ -33,16 +34,18 @@ ceros_biseccion(f, dom::Interval; tol::AbstractFloat=1/1024, nmax::Int = 15)
 
 Busca los ceros de `f` usando el método de bisección intervalar, dentro del
 intervalo `dom`. El argumento `tol` fija la tolerancia, es decir, el ancho
-máximo de cada intervalo candidato a contener ceros.
+máximo de cada intervalo candidato a contener ceros. `nmax` es el número de
+iteraciones máximo, y sirve para asegurarnos que el ciclo `while` termina.
 """
-function ceros_biseccion(f, dom::Interval; tol::AbstractFloat=1/1024)
+function ceros_biseccion(f, dom::Interval; tol::AbstractFloat=1/1024, nmax::Int = 15)
     bz = !(0 ∈ f(dom))
     v_candidatos = [dom]  # Este vector incluirá al resultado
 
     #Bifurca sucesivamente hasta que se cumpla la condición de salida
-    while !bz
+    its = 0 # número de iteraciones
+    while !bz && its ≤ nmax
         biseccion!(v_candidatos, f, tol)
-        bz = diam.(v_candidatos[1]) < tol # Esto evita problemas
+        bz = diam(v_candidatos[1]) < tol # Esto evita problemas
     end
 
     #Filtra los intervalos que no incluyan al 0
@@ -104,7 +107,7 @@ raices = ceros_biseccion(f, dom, tol=1/2.0^10)
 # Verificamos...
 all(0 .∈ f.(raices))
 
-# La raíz 0 está incluida en dos intervalos, en `raices[4]` y `raices[5]`, precisamente en uno de los bordes. Esto se debe a que el dominio usado es simétrico respecto a 0, y además $f(0)=0$. Entonces, si uno rehace el ejercicio, con un dominio ligeramente asimétrico, las 8 raíces aparecen con claridad.
+# La raíz 0 está incluida en dos intervalos, en `raices[4]` y `raices[5]`, precisamente en uno de los bordes. Esto se debe a que el dominio inicial usado es simétrico respecto a 0, y además $f(0)=0$. Entonces, si uno rehace el ejercicio, con un dominio ligeramente asimétrico, las 8 raíces aparecen con claridad.
 
 ceros_biseccion(f, Interval(-10,10.1), tol=1/2.0^10)
 
@@ -118,10 +121,11 @@ all(0 .∉ f′.(raices))
 
 # ---
 
-# ## El método de Newton para funciones reales
+## El método de Newton para funciones reales
 
 # A partir de ahora, supondremos que la función es diferenciable. Iniciaremos describiendo el método de Newton para funciones reales, para distinguirlo del caso que explota aritmética de intervalos.
 
+#-
 # Sea $f:[x]\to\mathbb{R}$ una función continua y diferenciable en $[x]$, y $x^*\in[x]$ una raíz de $f(x)$, es decir, $f(x^*)=0$. El método de Newton (usual) consiste en, a partir de un valor inicial $x_0\in[x]$ (suficientemente cercano a $x^*$), encontrar la intersección con el eje $x$ de la tangente a $f(x)$ que pasa por $(x_0, f(x_0))$. La tangente viene dada por $t(x) = f(x_0) + f^\prime(x_0) (x - x_0)$, por lo que su intersección con el eje x es
 # $$
 # x_1 = x_0 - \frac{f(x_0)}{f^\prime(x_0)}.
@@ -139,7 +143,7 @@ all(0 .∉ f′.(raices))
 # con $C>0$ constante.
 
 #-
-# Si las hipótesis del teorema no se cumplen, y en particular la condición sobre la cercanía entre el valor inicial y la raíz es muy importante, la secuencia entonces puede no converger. Esto puede ocurrir cuando los iterados forman una órbita periódica, o simplemente si saltan de una manera errática.
+# Si las hipótesis del teorema no se cumplen, en particular la condición sobre la cercanía entre el valor inicial y la raíz es muy importante y no es sencillo verificarla, la secuencia entonces puede no converger. Esto puede ocurrir cuando los iterados forman una órbita periódica, o simplemente si saltan de una manera errática.
 
 #-
 # Para ilustrar el método, consideraremos la función $f(x)=\sin(x) - 0.1*x^2 + 1$, cuya derivada es $f^\prime(x)=\cos(x) - 0.2*x$, y como punto inicial consideraremos $x_0=1.75$.
@@ -180,12 +184,16 @@ x₀
 # Claramente, cada nuevo iterado se acerca más y más a *una* raíz de $f(x)$, que en el caso que se ilustra es próximo a $\pi$. Sin embargo, uno puede verificar que si uno cambia ligeramente la condición inicial $x_0$, los iterados *pueden* converger a otra raíz, e incluso no converger. El punto es, en el mejor de los casos, el método de Newton *usual* permite obtener una raíz de $f(x)$, sin dar más información que eso.
 
 
+#-
 # ### Explotando *diferenciación automática*
 
+#-
 # En lo que hemos hecho arriba, una *incomodidad* es tener que proveer no sólo $f(x)$ sino además su derivada $f^\prime(x)$. Esto abre la posibilidad de cometer errores, ya sea en el cálculo de $f^\prime(x)$, o en su implementación. Una manera posible de salir de esta situación es explotando los métodos de [*diferenciación automática*](https://en.wikipedia.org/wiki/Automatic_differentiation).
 
+#-
 # Sin entrar mucho en los detalles, lo que esencialmente se hace para implementar diferenciación automática es definir una estructura apropiada que contiene dos campos, el de la función evaluada en el punto de interés, y el de su derivada evaluada en el mismo punto. Al igual que lo hicimos con la estructura `Intervalo`, uno debe sobrecargar las operaciones aritméticas y también las funciones estándar. Esto no lo haremos aquí, por falta de tiempo, y simplemente explotaremos que este tipo de cosas ya existe en Julia, utilizando en concreto la paquetería [`ForwardDiff.jl`](https://github.com/JuliaDiff/ForwardDiff.jl).
 
+#-
 # A continuación usaremos `ForwardDiff.jl` en el ejemplo que desarrollamos antes, lo que permite simplificar la implementación del método de Newton (dentro de ciertos límites).
 
 using ForwardDiff
@@ -224,13 +232,15 @@ gif(anim1b, "anim1b.gif", fps = 1)
 
 # ## El método de Newton para intervalos
 
-# Igual que para el método usual de Newton, supondremos que $f:[x]\to\mathbb{R}$ es continuamente diferenciable en $[x]$, que $x^*\in[x]$ satisface $f(x^*)=0$, que existe y está bien definida la extensión intervalar de $f^\prime(x)$, y que $0\notin F^\prime([x])$. Las hipótesis que hemos hecho permiten usar el Teorema del Valor Medio, y entonces tenemos que $f(x) = f(x^*)+f^\prime(\zeta)(x-x^*)$, para alguna $\zeta$ entre $x$ y $x^*$. De aquí, podemos despejar $x^*$,
+#-
+# Igual que para el método usual de Newton, supondremos que $f:[x]\to\mathbb{R}$ es continuamente diferenciable en $[x]$, que $x^*\in[x]$ satisface $f(x^*)=0$, que existe y está bien definida la extensión intervalar de $f^\prime(x)$, y que $0\notin F^\prime([x])$. Las hipótesis que hemos hecho permiten usar el Teorema del Valor Medio, y entonces escribimos
+# $$f(x) = f(x^*)+f^\prime(\zeta)(x-x^*),$$
+# para alguna $\zeta$ entre $x$ y $x^*$, es decir, que está contenida en $[x]$. De esta expresión, despejamos $x^*$,
 # $$
 # x^* = x - \frac{f(x)}{f^\prime(\zeta)} \in x - \frac{f(x)}{F^\prime([x])} \equiv N([x]; x).
 # $$
-
-#-
-# Dado que $x^*\in[x]$, entonces tenemos además que $x^*\in N([x]; x) \cap [x]$, lo que se cumple *para toda* $x\in[x]$. La cota obtenida al considerar $x=m=\textrm{mid}([x])$ se conoce como *operador de Newton*:
+# Esta expresión demuestra en particular que $x^*\in N([x],x)$, y dado que $x^*\in[x]$, entonces tenemos además que $x^*\in N([x]; x) \cap [x]$. La cota obtenida al considerar $x=m=\textrm{mid}([x])$
+# se conoce como *operador de Newton*:
 # $$
 # N([x]) \equiv N([x];m) = m - \frac{f(m)}{f^\prime(\zeta)}.
 # $$
@@ -241,10 +251,12 @@ gif(anim1b, "anim1b.gif", fps = 1)
 # [x_{k+1}] = N([x_k])\cap [x_k], k=0,1,2,\dots.
 # $$
 
+#-
 # A manera de ilustración del método de Newton intervalar, consideraremos la función $g(x) = \textrm{atan}(x)$ en el intervalo $[x]=[-1,3]$, que incluye a la única raíz de la función, $x^*=0$.
 
 using IntervalArithmetic
 
+#-
 g(x) = atan(x)
 dom_tot = -10.5:0.125:10.5
 dom0 = -1 .. 3
@@ -265,7 +277,7 @@ Se asume (y verifica) que 0 ∉ f′.
 function N(f, dom)
     f′ = ForwardDiff.derivative(f, dom)
     @assert 0 ∉ f′
-      m = mid(dom)
+    m = mid(dom)
     md = m .. m
     Nm = md - f(md)/f′
     return Nm
@@ -315,10 +327,13 @@ plot!([xmid, xmid], [y0, g_xmid], color=:gray, linestyle=:dot, markershape=:circ
 
 # Claramente, si $x^*\in [x_0]$, entonces, $x^*\in [x_k]$ para toda $k$. Por otro lado, si $[x_0]$ no contiene ninguna raíz, típicamente obtendremos $N([x_k])\cap [x_k] = \emptyset$, lo que tiene una interpretación clara y directa.
 
+#-
 # Las observaciones anteriores son consecuencia de los siguientes resultados.
 
+#-
 # **Teorema** (Método de Newton intervalar). Supongamos que $N([x_0])$ está bien definido. Si $[x_0]$ contiene una raíz $x^*$ de $f$, entonces para todos los iterados se tiene que $x^*\in [x_k]$. Además, los iterados forman una secuencia anidada que converge a $x^*$.
 
+#-
 # **Teorema**. Sea $f\in C^2([x], \mathbb{R})$, y supongamos que $N([x])$ está bien definido. Entonces:
 #
 # 1. si $N([x])\cap [x] = \emptyset$, entonces $[x]$ no contiene a ninguna raíz de $f$;
@@ -332,7 +347,7 @@ plot!([xmid, xmid], [y0, g_xmid], color=:gray, linestyle=:dot, markershape=:circ
 # Hasta ahora hemos requerido que el intervalo $[x]$, en el que usamos el método de Newton intervalar, y la función $f(x)$ sean tales que $0\notin f^\prime([x])$. El caso en que $0\in f^\prime([x])$ da problemas con el método intervalar de Newton ya que, al dividir entre un intervalo que contiene a 0 (sin que sea idéntico a 0), el operador de Newton corresponde a $[-\infty, \infty]$, y por lo tanto $N([x])\cap[x]=[x]$. Sin embargo, esto puede resolverse reemplazando la división que aparece en la definición del operador de Newton por la *división extendida* de intervalos. Esta situación aparece, por ejemplo, cuando la función $f$ tiene máximos o mínimos en el intervalo $[x]$.
 
 #-
-# La división extendida puede devolver uno o dos intervalos (disconexos). Entonces, el método de Newton intervalar extendido consiste en el método de Newton intervalar aplicado a cada uno de los intervalos producidos por la división extendida, en cada iteración del método.
+# La división extendida devuelve en general dos intervalos, uno de los cuales puede ser vacío. Entonces, el método de Newton intervalar extendido consiste en el método de Newton intervalar aplicado a cada uno de los intervalos producidos por la división extendida, en cada iteración del método.
 
 #-
 # Para ilustrar la aplicación del método de Newton intervalar extendido, consideraremos la función $f(x)= \sin(x) - 0.1*x^2 + 1$, en el intervalo $[-6,6]$, que fue el ejemplo con el que ilustramos, y criticamos, el método usual de Newton.
@@ -351,6 +366,7 @@ function N_extdiv(f, dom)
     Nm = md .- [extended_div(f(md), f′)...]
     return Nm
 end
+
 
 #-
 dom_tot = -10:0.125:10
@@ -398,10 +414,10 @@ plot!([xmid, xmid], [y0, f_xmid], color=:gray, linestyle=:dot, markershape=:circ
     label=nothing, alpha=0.8)
 
 #-
-# Como se observa en la figura anterior, la división extendida produce dos intervalos disconexos que se extienden a infinito. Sin embargo, gracias a la intersección con el intervalo inicial, los dos intervalos que resultan $[x_1^{(a)}]$ y $[x_1^{(b)}]$ contienen a *todas* las raíces de la función $f(x)$. Más aún, el hecho de que para el intervalo de la derecha se cumpla $[x_1^{(b)}]\subseteq [x_0]$ indica, como se observa, que el intervalo contiene a una única raíz (simple) de $f(x)$.
+# Como se observa en la figura anterior, la división extendida produce dos intervalos disconexos que se extienden a infinito. Sin embargo, gracias a la intersección con el intervalo inicial, los dos intervalos que resultan $[x_1^{(a)}]$ y $[x_1^{(b)}]$ contienen a *todas* las raíces de la función $f(x)$.
 
 #-
-# Esto ilustra cómo el método de Newton intervalar extendido permite desechar intervalos que no tienen raíces de la función $f(x)$, y cómo es posible obtener todos intervalos que contienen a *todas* las raíces dentro del intervalo $[x]$.
+# Esto ilustra cómo el método de Newton intervalar extendido permite desechar intervalos que no tienen raíces de la función $f(x)$, y cómo es posible obtener todos intervalos que contienen a *todas* las raíces dentro del intervalo $[x]$. Esta última observación ilustra un método más general y con muchas aplicaciones: *branch-and-prune*.
 
 # ---
 
